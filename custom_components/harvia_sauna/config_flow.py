033d0977e769a9f51a1164333bfa1ff3d2304bfd
Andrew Atkinson <72718533+ajs-atkinson@users.noreply.github.com>
2026-06-16 13:20:20 +1000

    feat: options flow to set stove rated power (kW)

--- custom_components/harvia_sauna/config_flow.py
+++ custom_components/harvia_sauna/config_flow.py
@@ -2,33 +2,32 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+from homeassistant.helpers.selector import (
+    NumberSelector, NumberSelectorConfig, NumberSelectorMode,
+)
 from .api import HarviaSaunaAPI
-from .constants import DOMAIN  # Ensure constants.py defines your DOMAIN
+from .constants import DOMAIN
+
+CONF_STOVE_RATED_POWER = "stove_rated_power_kw"
+
 
 class HarviaSaunaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for your_component."""
+    """Handle a config flow for Harvia Sauna."""
 
-    VERSION = 1  # Config flow version
+    VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input=None):
-        """Handle a flow initiated by the user."""
         errors = {}
-
-        # Check if we already have a configuration
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
-
-        # Process submitted data
         if user_input is not None:
-            # Validate credentials
-            api =  HarviaSaunaAPI(user_input[CONF_USERNAME], user_input[CONF_PASSWORD],self.hass)
+            api = HarviaSaunaAPI(user_input[CONF_USERNAME], user_input[CONF_PASSWORD], self.hass)
             valid = await api.authenticate()
             if valid:
                 return self.async_create_entry(title="Harvia Sauna", data=user_input)
             else:
                 errors["base"] = "invalid_auth"
-
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
@@ -37,33 +36,28 @@ class HarviaSaunaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             }),
             errors=errors,
         )
+
     @staticmethod
-    def async_get_options_flow(config_entry: config_entries.ConfigEntry) -> config_entries.OptionsFlow:
+    @callback
+    def async_get_options_flow(config_entry):
         return HarviaSaunaOptionsFlowHandler(config_entry)
 
+
 class HarviaSaunaOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
+    """Options flow: set the stove rated power so stove energy (kWh) can be derived."""
+
+    def __init__(self, config_entry):
         self._config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
-        """Manage options."""
-        errors = {}
-
         if user_input is not None:
-            # Validate credentials
-            api =  HarviaSaunaAPI(user_input[CONF_USERNAME], user_input[CONF_PASSWORD],self.hass)
-            valid = await api.authenticate()
-            if valid:
-                return self.async_create_entry(title="Harvia Sauna", data=user_input)
-            else:
-                errors["base"] = "invalid_auth"
-
+            return self.async_create_entry(title="", data=user_input)
+        current = self._config_entry.options.get(CONF_STOVE_RATED_POWER, 0)
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Required(CONF_USERNAME, default=self._config_entry.data.get(CONF_USERNAME)): str,
-                vol.Required(CONF_PASSWORD, default=self._config_entry.data.get(CONF_PASSWORD)): str,
+                vol.Optional(CONF_STOVE_RATED_POWER, default=current): NumberSelector(
+                    NumberSelectorConfig(min=0, max=30, step=0.1, mode=NumberSelectorMode.BOX, unit_of_measurement="kW")
+                ),
             }),
-            errors=errors
         )
